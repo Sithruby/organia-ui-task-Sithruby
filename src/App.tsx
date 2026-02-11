539191a30316d27f86d4d6092db44d7f679157ca
Sithruby <sithruby@gmail.com>
2026-02-12 00:29:52 +0530

    #1, project initialized with React and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "./Components/sidebar"
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // ✅ include ts and tsx
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
